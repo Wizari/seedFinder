44fbfb64829dd33c922cc592c9a12eca39ed9810
I.baranov <i.baranov@octaviangameart.ru>
2025-11-27 17:15:52 +0300

    add ArgsProcessor

--- src/main/kotlin/com/gmail/wizaripost/seedFinder/SeedFinderApplication.kt
+++ src/main/kotlin/com/gmail/wizaripost/seedFinder/SeedFinderApplication.kt
@@ -1,6 +1,7 @@
 package com.gmail.wizaripost.seedFinder
 
 import com.gmail.wizaripost.seedFinder.seedfinder.SeedRunner
+import com.gmail.wizaripost.seedFinder.service.ArgsProcessor
 import org.springframework.beans.factory.getBean
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -15,9 +16,11 @@ fun main(args: Array<String>) {
     val application = runApplication<SeedFinderApplication>(*args);
 
     val seedRunner = application.getBean<SeedRunner>()
+    val argsProcessor = application.getBean<ArgsProcessor>()
 //    val argsArr : Array<String> = arrayOf("17179869625")
     val argsArr : Array<String> = arrayOf("623927852")
 
-    seedRunner.run(argsArr[0].toULong())
+
+    seedRunner.run(argsProcessor.parseFirstArgToULong(argsArr))
 //    seedRunner.run(args[0].toULong())
 }
