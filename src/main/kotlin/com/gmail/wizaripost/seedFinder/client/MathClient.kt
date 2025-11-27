c39599a24100d50d9f6047db18f5c15ed631b405
I.baranov <i.baranov@octaviangameart.ru>
2025-11-27 15:57:40 +0300

    add Close

--- src/main/kotlin/com/gmail/wizaripost/seedFinder/client/MathClient.kt
+++ src/main/kotlin/com/gmail/wizaripost/seedFinder/client/MathClient.kt
@@ -42,33 +42,3 @@ interface MathClient {
         request: CloseRequest
     ): String
 }
-
-//import com.bar.seedFinder.dto.*
-//import org.springframework.cloud.openfeign.FeignClient
-//import org.springframework.http.MediaType
-//import org.springframework.web.bind.annotation.*
-//
-//@FeignClient(name = "MathClient", url = "http://localhost:8012")
-//interface MathClient {
-//
-//    @PostMapping(
-//        value = ["/RumblingRun-variation-95/newGame"],
-//        consumes = [MediaType.APPLICATION_JSON_VALUE], // Или application/vnd.api+json если строго нужно
-//        produces = [MediaType.APPLICATION_JSON_VALUE]
-//    )
-//    fun newGame(@RequestBody request: NewGameRequest): GameResponse
-//
-//    @PostMapping(
-//        value = ["/RumblingRun-variation-95/getConfig"],
-//        consumes = [MediaType.APPLICATION_JSON_VALUE],
-//        produces = [MediaType.APPLICATION_JSON_VALUE]
-//    )
-//    fun getConfig(): ConfigResponse
-//
-//    @PostMapping(
-//        value = ["/RumblingRun-variation-95/execute"],
-//        consumes = [MediaType.APPLICATION_JSON_VALUE],
-//        produces = [MediaType.APPLICATION_JSON_VALUE]
-//    )
-//    fun execute(@RequestBody request: ExecuteRequest): GameResponse
-//}
