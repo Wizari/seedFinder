778f5a0f76cb45474406cece1cee9ebbcb6fff98
Igor Simagin <i.simagin@octaviangameart.ru>
2025-11-28 11:39:10 +0300

    RoundStage

--- src/main/kotlin/com/gmail/wizaripost/seedFinder/SeedFinderApplication.kt
+++ src/main/kotlin/com/gmail/wizaripost/seedFinder/SeedFinderApplication.kt
@@ -1,7 +1,14 @@
 package com.gmail.wizaripost.seedFinder
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.gmail.wizaripost.seedFinder.client.MathClient
+import com.gmail.wizaripost.seedFinder.dto.ConfigResponse
+import com.gmail.wizaripost.seedFinder.dto.GameResponse
 import com.gmail.wizaripost.seedFinder.seedfinder.SeedRunner
 import com.gmail.wizaripost.seedFinder.service.ArgsProcessor
+import com.gmail.wizaripost.seedFinder.service.actions.NewGameService
+import com.gmail.wizaripost.seedFinder.service.stages.ActionBuilder
 import org.springframework.beans.factory.getBean
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -17,10 +24,31 @@ fun main(args: Array<String>) {
 
     val seedRunner = application.getBean<SeedRunner>()
     val argsProcessor = application.getBean<ArgsProcessor>()
+    val newGameService= application.getBean<NewGameService>()
+    val objectMapper = application.getBean<ObjectMapper>()
+    val mathClient = application.getBean<MathClient>()
+
 //    val argsArr : Array<String> = arrayOf("17179869625")
     val argsArr : Array<String> = arrayOf("623927852")
+    val seed = argsProcessor.parseFirstArgToULong(argsArr)
+
+    // Шаг 1: NewGame
+    val gameId = "RumblingRun-variation-95"
+    val responseNewGameString = newGameService.execute(gameId, seed)
+    val newGameResponse: GameResponse = objectMapper.readValue(responseNewGameString)
 
 
-    seedRunner.run(argsProcessor.parseFirstArgToULong(argsArr))
+
+    // Шаг 2: GetConfig
+    val responseGetConfigString = mathClient.getConfig(gameId)
+    val configResponse: ConfigResponse = objectMapper.readValue(responseGetConfigString)
+//        resultPostProcessor.process("GetConfig", configResponse)
+
+    for (i in 0..10) {
+        val responseNewGameString = newGameService.execute(gameId, seed)
+        val newGameResponse: GameResponse = objectMapper.readValue(responseNewGameString)
+
+        seedRunner.run(gameId, newGameResponse)
+    }
 //    seedRunner.run(args[0].toULong())
 }
--- src/main/kotlin/com/gmail/wizaripost/seedFinder/seedfinder/SeedRunner.kt
+++ src/main/kotlin/com/gmail/wizaripost/seedFinder/seedfinder/SeedRunner.kt
@@ -1,14 +1,7 @@
 package com.gmail.wizaripost.seedFinder.seedfinder
 
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.gmail.wizaripost.seedFinder.client.MathClient
-import com.gmail.wizaripost.seedFinder.dto.ConfigResponse
 import com.gmail.wizaripost.seedFinder.dto.GameResponse
-import com.gmail.wizaripost.seedFinder.service.ResultPostProcessor
-import com.gmail.wizaripost.seedFinder.service.actions.NewGameService
-import com.gmail.wizaripost.seedFinder.service.stages.ActionBuilder
 import com.gmail.wizaripost.seedFinder.service.stages.RoundStage
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -16,47 +9,22 @@ import org.springframework.stereotype.Component
 
 @Component
 class SeedRunner(
-    private val mathClient: MathClient,
-    private val newGameService: NewGameService,
-    private val resultPostProcessor: ResultPostProcessor,
-    private val objectMapper: ObjectMapper,
     private val roundStage: Set<RoundStage>,
-    private val actionBuilder: ActionBuilder,
-    ) {
+) {
 
     private val logger = LoggerFactory.getLogger(SeedRunner::class.java)
 
 
-
-    fun run(seed: ULong) {
-
-        var response: GameResponse? = null
-        // Шаг 1: NewGame
-        val gameId = "RumblingRun-variation-95"
-        val responseNewGameString = newGameService.execute(gameId, seed)
-        val newGameResponse: GameResponse = objectMapper.readValue(responseNewGameString)
-
-//        resultPostProcessor.process("NewGame", newGameResponse)
-        var action: String = actionBuilder.getFirstAction(responseNewGameString)
-//        logger.info("Executing command: $action")
-        response = newGameResponse
-
-        // Шаг 2: GetConfig
-        val responseGetConfigString = mathClient.getConfig(gameId)
-        val configResponse: ConfigResponse = objectMapper.readValue(responseGetConfigString)
-//        resultPostProcessor.process("GetConfig", configResponse)
-
+    fun run(gameId: String, payload: GameResponse?) {
+        var response: GameResponse? = payload
+        var action = "Spin"
         do {
-
             val stage = roundStage.find { it.valid(action) } ?: throw RuntimeException("Unknow stage $action")
             val gameResponse = response ?: throw RuntimeException("Response can't be null")
             val stageResponse = stage.execute(mapOf("gameId" to gameId, "payload" to gameResponse))
             action = stageResponse.nextAction
             response = stageResponse.response
-
-
         } while (action != "Spin")
-        logger.info("Game round completed for seed: $seed")
     }
 
 }
