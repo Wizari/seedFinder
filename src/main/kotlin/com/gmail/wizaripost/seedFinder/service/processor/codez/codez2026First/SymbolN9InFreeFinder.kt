98c7e7366b81c28345ae33ab20947e1e06a2e947
I.baranov <i.baranov@octaviangameart.ru>
2026-02-19 09:51:31 +0300

    CodeZ SymbolN9InFreeFinder

---
+++ src/main/kotlin/com/gmail/wizaripost/seedFinder/service/processor/codez/codez2026First/SymbolN9InFreeFinder.kt
@@ -0,0 +1,51 @@
+package com.gmail.wizaripost.seedFinder.service.processor.codez.codez2026First
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.gmail.wizaripost.seedFinder.dto.GameStateResponse
+import com.gmail.wizaripost.seedFinder.logging.LoggingService
+import com.gmail.wizaripost.seedFinder.service.processor.ResultPostProcessor
+import com.gmail.wizaripost.seedFinder.service.processor.utils.Utils
+import org.springframework.stereotype.Service
+
+
+/*
+* [Code Z] Найти сид c [9] символом >= Х2 во FreeSpin
+*/
+
+@Service
+class SymbolN9InFreeFinder(private val om: ObjectMapper, private val utils: Utils) : LoggingService(),
+    ResultPostProcessor {
+    override fun process(key: String, payload: Any) {
+        if (key == "Spin") {
+            return
+        }
+        val resp: GameStateResponse = om.readValue(payload as String)
+
+//        val matrix = data.gameRound?.gameState?.Cascade?.stg[0].cntnt
+        val matrices = resp.result?.gameState?.public?.Cascade?.stg
+
+
+        if (matrices == null) {
+            return
+        }
+        var symbol = 0
+        for (matrix in matrices) {
+            symbol = 0
+            for (reel in matrix.cntnt!!) {
+                for (row in reel) {
+                    if (row == 9) {
+                        symbol++
+                    }
+                }
+
+            }
+
+        }
+        if (symbol >= 2) {
+            val seed = resp.result?.gameState?.private?.modelCore?.seed
+            println("[$symbol] $seed")
+            logSeed("[$symbol] $seed")
+        }
+    }
+}
